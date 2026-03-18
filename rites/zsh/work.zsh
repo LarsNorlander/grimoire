c098d7c86e6f833b0ae00e7f93e1e5d4bb119844
Lars Norlander <lars.nrlndr@gmail.com>
2026-03-18 22:46:10 +0100

    Add OpenSpec shell completions to work profile
    Move the OpenSpec completions config from drifted zshrc into
    work.zsh where it belongs.

--- rites/zsh/work.zsh
+++ rites/zsh/work.zsh
@@ -3,6 +3,11 @@
 # mysql (installed by work.nix via Homebrew)
 export PATH="/opt/homebrew/opt/mysql-client@8.4/bin:$PATH"
 
+# openspec
+fpath=("$HOME/.oh-my-zsh/custom/completions" $fpath)
+autoload -Uz compinit
+compinit
+
 # sdkman
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
